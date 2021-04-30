eff421ff033f8c076b5c3b1dd8b5b20dbaea5f70
nikhil218 <nikhil.tirpude317@gmail.com>
2021-05-01 00:24:09 +0530

    added code to claculate monthly wage till a condition is met

--- empWage.java
+++ empWage.java
@@ -4,36 +4,29 @@ public class empWage {
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HOUR = 20;
 	public static final int MAX_WORKING_DAYS = 20;
-	public static final int MAX_WORKING_HOURS = 50;
+	public static final int MAX_WORKING_HOURS = 100;
 
 	static int totalEmpWage;
-
-	 public static int getHours(){
-                int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-                int empHours;
-
-                switch(empCheck) {
-                        case IS_FULL_TIME :
-                                empHours = 8;
-                                break;
-                        case IS_PART_TIME :
-                                empHours = 4;
-                                break;
-                        default :
-                                empHours = 0;
-                        }
-                return empHours;
-        }
 
 	public static int calculate_wage() {
 		int  totalWorkingDays = 0;
 		int totalEmpHours = 0;
 
 		while( totalWorkingDays != MAX_WORKING_DAYS && totalEmpHours <= MAX_WORKING_HOURS) {
-			int empWage;
+			int empHours, empWage;
 			totalWorkingDays++;
-			int empHours = getHours();
 
+			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+				switch(empCheck) {
+					case IS_FULL_TIME :
+						empHours = 8;
+						break;
+					case IS_PART_TIME :
+						empHours = 4;
+						break;
+					default :
+						empHours = 0;
+				}
 			empWage = empHours * EMP_WAGE_PER_HOUR;
 			System.out.println("Day " + totalWorkingDays + " Wage : " + empWage);
 			totalEmpWage += empWage;
