c67540f718b7960eb074f578d2f19bf959cbf74b
nikhil218 <nikhil.tirpude317@gmail.com>
2021-05-01 00:07:01 +0530

    Added code with additional option of employee working part time

--- empWage.java
+++ empWage.java
@@ -1,16 +1,21 @@
 public class empWage{
 
    public static final int EMP_WAGE_PER_HOUR = 20;
+   public static final int IS_FULL_TIME = 1;
+   public static final int IS_PART_TIME = 2;
 
    public static void calculate_wage(){
 
       int empWage, empHour ;
 
-      int check = (int)Math.floor(Math.random() * 10) % 2;
+      int check = (int)Math.floor(Math.random() * 10) % 3;
 
-      if (check == 1){
+      if (check == IS_FULL_TIME){
          System.out.println("Employee is present");
          empHour = 8 ;
+      }
+      else if( check == IS_PART_TIME){
+         empHour = 4;
       }
       else{
          System.out.println("Employee is absent");
