7c5f395b5fd3d6f90d145b52cd81fb6e983d766a
nikhil218 <nikhil.tirpude317@gmail.com>
2021-05-01 00:11:33 +0530

    added code to calculate total monthly wage

--- empWage.java
+++ empWage.java
@@ -1,33 +1,42 @@
-public class empWage {
+public class empWage{
 
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
-	public static final int EMP_WAGE_PER_HOUR = 20;
+   public static final int EMP_WAGE_PER_HOUR = 20;
+   public static final int IS_FULL_TIME = 1;
+   public static final int IS_PART_TIME = 2;
+   public static final int WORKING_DAYS_PER_MONTH = 20;
 
-	public static void calculate_wage() {
-		int empHours, empWage;
+   static int totalEmpWage;
 
-		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-		switch(empCheck) {
-			case IS_FULL_TIME :
-				System.out.println("Employee is Full Time");
-				empHours = 8;
-				break;
-			case IS_PART_TIME :
-				System.out.println("Employee is Part Time");
-				empHours = 4;
-				break;
-			default :
-				System.out.println("Employee is Absent");
-				empHours = 0;
-		}
-		empWage = empHours * EMP_WAGE_PER_HOUR;
-		System.out.println("Daily Wage of an Employee is : " + empWage);
-	}
+   public static void calculate_wage(){
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation problem using OOPs");
+      int empWage, empHour ;
 
-		calculate_wage();
-	}
+      for (int day=1; day<=WORKING_DAYS_PER_MONTH; day++){
+
+         int check = (int)Math.floor(Math.random() * 10) % 3;
+
+         if (check == IS_FULL_TIME){
+            System.out.println("Employee is present full time");
+            empHour = 8 ;
+         }
+         else if( check == IS_PART_TIME){
+            empHour = 4;
+            System.out.println("Employee is present part time");
+         }
+         else{
+            System.out.println("Employee is absent");
+            empHour = 0 ;
+         }
+         empWage = EMP_WAGE_PER_HOUR*empHour;
+         totalEmpWage += empWage;
+      }
+
+      System.out.println("Monthly Wage of an Employee is : " + totalEmpWage);
+   }
+
+   public static void main(String[] args){
+      System.out.println("Welcome to Employee Wage Computation problem using OOPs");
+
+      calculate_wage();
+   }
 }
