317e95951dd749f8a15740bc3d5acdc969d67724
nikhil218 <nikhil.tirpude317@gmail.com>
2021-05-01 00:09:24 +0530

    Added switch statement

--- empWage.java
+++ empWage.java
@@ -1,33 +1,33 @@
-public class empWage{
+public class empWage {
 
-   public static final int EMP_WAGE_PER_HOUR = 20;
-   public static final int IS_FULL_TIME = 1;
-   public static final int IS_PART_TIME = 2;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_WAGE_PER_HOUR = 20;
 
-   public static void calculate_wage(){
+	public static void calculate_wage() {
+		int empHours, empWage;
 
-      int empWage, empHour ;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		switch(empCheck) {
+			case IS_FULL_TIME :
+				System.out.println("Employee is Full Time");
+				empHours = 8;
+				break;
+			case IS_PART_TIME :
+				System.out.println("Employee is Part Time");
+				empHours = 4;
+				break;
+			default :
+				System.out.println("Employee is Absent");
+				empHours = 0;
+		}
+		empWage = empHours * EMP_WAGE_PER_HOUR;
+		System.out.println("Daily Wage of an Employee is : " + empWage);
+	}
 
-      int check = (int)Math.floor(Math.random() * 10) % 3;
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation problem using OOPs");
 
-      if (check == IS_FULL_TIME){
-         System.out.println("Employee is present");
-         empHour = 8 ;
-      }
-      else if( check == IS_PART_TIME){
-         empHour = 4;
-      }
-      else{
-         System.out.println("Employee is absent");
-         empHour = 0 ;
-      }
-      empWage = EMP_WAGE_PER_HOUR*empHour;
-      System.out.println("Daily Wage of an Employee is : " + empWage);
-   }
-
-   public static void main(String[] args){
-      System.out.println("Welcome to Employee Wage Computation problem using OOPs");
-
-      calculate_wage();
-   }
+		calculate_wage();
+	}
 }
